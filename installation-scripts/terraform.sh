a41f2be0cdfd33d5ed209dd332ee2a376ab81fa4
iusetabs <14316461@mail.dcu.ie>
2023-02-25 13:55:37 +0000

    Update terraform script for amd64

--- installation-scripts/terraform.sh
+++ installation-scripts/terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installs terraform 
+# Installs terraform
 
 echo "Installing terraform"
 
@@ -8,10 +8,10 @@ architecture="$(dpkg --print-architecture)"
 apt-get --assume-yes install unzip
 
 
-if [[ "$architecture" == "arm64" ]]
+if [ "$architecture" == "arm64" ]  || [ "$architecture" == "amd64" ];
 then
     wget https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_"$architecture".zip
-    unzip terraform_1.3.9_linux_arm64
+    unzip terraform_1.3.9_linux_"$architecture"
     mv terraform /usr/local/bin/
     terraform --version
 fi
